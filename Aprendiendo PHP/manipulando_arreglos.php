e016a3a8a4befa6f40577a6be3cf5ccf67c7d61f
Fernando Acostupa <cloudchaser2000@gmail.com>
2022-09-08 23:48:08 -0500

    Practicando HTML

---
+++ Aprendiendo PHP/manipulando_arreglos.php
@@ -0,0 +1,17 @@
+
+
+<?php
+# Arreglos en: https://www.php.net/manual/es/book.array.php
+$edades = [18,22,40,34];
+$contar = count($edades);
+
+echo "Edades tiene ".$contar." Elementos";
+
+
+
+
+
+?>
+
+
+
