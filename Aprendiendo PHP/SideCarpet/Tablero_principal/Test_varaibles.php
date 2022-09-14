1ab26156f7108fca889d66554c598ba297890c6a
Fernando Acostupa <cloudchaser2000@gmail.com>
2022-09-14 11:54:34 -0500

    Ordenando Carpetas

---
+++ Aprendiendo PHP/SideCarpet/Tablero_principal/Test_varaibles.php
@@ -0,0 +1,13 @@
+<?php
+
+$color = 'verde';
+$fruta = 'manzana';
+
+// Probando Variables Horas , minutos y fecha , esto se sacara de la base de dats pero lo hago antes de hacer la integracion+
+// 
+
+
+$horas1 = "5 horas";
+$horasprevias = "7horas";
+
+?>
